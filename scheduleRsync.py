87938d650effff1d78f8e6ccafe4430bec4e9de9
mziegler <mattpziegler@gmail.com>
2026-03-31 17:41:17 -0500

    add folders with deployment names

--- scheduleRsync.py
+++ scheduleRsync.py
@@ -7,7 +7,7 @@ expandedPhotosPath = os.path.expanduser(MOTHBOX_PHOTOS_DIRECTORY_PATH)
 expandedLogPath = os.path.expanduser(MOTHBOX_RSYNC_LOG_PATH)
 
 """Load a CSV file containing mothbox information. Returns a list with one dict for each 
-mothbox; having the keys "name" and "hostOrIP."
+mothbox; having the keys "deploymentName", "mothboxName" and "hostOrIP."
 """
 def load_mothbox_list(filename='mothbox-list.csv'):
     with open(filename) as csvfile:
@@ -32,8 +32,8 @@ def schedule_frequent_photo_syncjobs(startHour, endHour):
         hour_list = [h%24 for h in range(startHour, endHour if endHour > startHour else endHour + 24)]
 
         job = cron.new(
-            command=f'rsync -rv --times --remove-source-files --mkpath {mothbox["hostOrIP"]}:/home/pi/Desktop/Mothbox/photos {os.path.join(expandedPhotosPath, mothbox["name"])}/ >> {expandedLogPath} 2>&1',
-            comment=f'Generated-Mothbox-Rsync job for {mothbox["name"]} at hour {hour}')
+            command=f'rsync -rv --times --remove-source-files --mkpath {mothbox["hostOrIP"]}:/home/pi/Desktop/Mothbox/photos/* {os.path.join(expandedPhotosPath, mothbox["deploymentName"])}/ >> {expandedLogPath} 2>&1',
+            comment=f'Generated-Mothbox-Rsync job for {mothbox["mothboxName"]}')
         job.hour.on(*hour_list)  # Schedule the job to run at the specified hour
         job.minute.every(1)  # Schedule the job to run every minute within the hour
 
@@ -56,7 +56,10 @@ def remove_generated_jobs():
 
 
 if __name__ == "__main__":
+    remove_generated_jobs()
     schedule_frequent_photo_syncjobs(18,6)
 
 
+# TODO
 # one option for removing empty directories: find . -type d -empty -delete
+# do a separate log for each Mothbox. Maybe a separate log for each date, also?
